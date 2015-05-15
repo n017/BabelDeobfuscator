12e57d90c66712cd52a84786daf4fcbdcc62c22d
Sam <samjam5376@gmail.com>
2015-05-15 02:40:46 +0200

    Very first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BabelDeobfuscator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse for assembly";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Execute";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 115);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(47, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(463, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox1DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextBox1DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name of Assembly:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(50, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(415, 77);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Remove demo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 166);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BabelDeobfuscator - XenocodeRCE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+
+namespace BabelDeobfuscator
+{
+    public partial class Form1 : Form
+    {
+        #region Declarations
+
+        public string DirectoryName = "";
+        public int ConstantKey;
+        public int ConstantNum;
+        public MethodDef Methoddecryption;
+        public TypeDef Typedecryption;
+        public MethodDef MethodeResource;
+        public TypeDef TypeResource;
+        public ModuleDefMD module;
+        public int x;
+        public int DeobedStringNumber;
+        public Mode value;
+        
+        #endregion
+
+        #region Designer
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void TextBox1DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void TextBox1DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".", StringComparison.Ordinal);
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            textBox1.Text = text;
+                            int num2 = text.LastIndexOf("\\", StringComparison.Ordinal);
+                            if (num2 != -1)
+                            {
+                                DirectoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (DirectoryName.Length == 2)
+                            {
+                                DirectoryName += "\\";
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            module = ModuleDefMD.Load(textBox1.Text);
+            FindStringDecrypterMethods(module);
+            DeobedStringNumber = 0;
+            DecryptStringsInMethod(module, Methoddecryption);
+            if (checkBox1.Checked)
+            {
+                RemoveDemo(module);
+            }
+            string text2 = Path.GetDirectoryName(textBox1.Text);
+            if (!text2.EndsWith("\\"))
+            {
+                text2 += "\\";
+            }
+            string path = text2 + Path.GetFileNameWithoutExtension(textBox1.Text) + "_patched" +
+                          Path.GetExtension(textBox1.Text);
+            var opts = new ModuleWriterOptions(module);
+            opts.Logger = DummyLogger.NoThrowInstance;
+            module.Write(path, opts);
+            label2.Text = "Successfully decrypted " + DeobedStringNumber + " strings !";
+            
+        }
+
+        #endregion
+
+        #region Method
+
+        public enum Mode
+        {
+            Trial,
+            Normal // soon to be added, if you have any sample to give, drop me a message ...
+        };
+
+        private void RemoveDemo(ModuleDefMD module)
+        {
+            foreach (var type in module.Types)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (method.Body.HasInstructions)
+                    {
+                        var instrs = method.Body.Instructions;
+                        if (instrs.Count > 6)
+                        {
+                            for (int i = 0; i < method.Body.Instructions.Count; i++)
+                            {
+                                if (method.Body.Instructions[i].OpCode == OpCodes.Call &&
+                                    method.Body.Instructions[i + 1].OpCode == OpCodes.Ldc_I8 &&
+                                    method.Body.Instructions[i + 2].OpCode == OpCodes.Newobj && 
+                                    method.Body.Instructions[i + 3].OpCode == OpCodes.Call &&
+                                    method.Body.Instructions[i + 4].OpCode == OpCodes.Brfalse_S &&
+                                    method.Body.Instructions[i + 5].OpCode == OpCodes.Newobj &&
+                                    method.Body.Instructions[i + 6].OpCode == OpCodes.Throw)
+                                {
+                                    for (int j = 0; j < 7  ;j++)
+                                        method.Body.Instructions.RemoveAt(i);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void FindStringDecrypterMethods(ModuleDefMD module)
+        {
+            foreach (var type in module.Types)
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (method.HasBody == false)
+                        continue;
+                    if (method.Body.HasInstructions)
+                    {
+                        var instrs = method.Body.Instructions;
+                        if (instrs.Count > 5)
+                        {
+                            for (int i = 0; i < instrs.Count - 3; i++)
+                            {
+                                if (instrs[i].OpCode.Code == Code.Call && instrs[1].OpCode.Code == Code.Ldc_I8 &&
+                                    instrs[2].OpCode.Code == Code.Newobj && instrs[3].OpCode.Code == Code.Call &&
+                                    instrs[4].OpCode.Code == Code.Brfalse_S && instrs[5].OpCode.Code == Code.Newobj &&
+                                    instrs[6].OpCode.Code == Code.Throw)
+                                {
+                                    Methoddecryption = method;
+                                    Typedecryption = type;
+                                    value = Mode.Trial;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void DecryptStringsInMethod(ModuleDefMD module, MethodDef Methoddecryption)
+        {
+            foreach (TypeDef type in module.Types)
+            {
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (!method.HasBody)
+                        break;
+                    for (int i = 0; i < method.Body.Instructions.Count; i++)
+                    {
+                        if (method.Body.Instructions[i].OpCode == OpCodes.Ldstr && method.Body.Instructions[i + 1].IsLdcI4() && method.Body.Instructions[i+2].OpCode == OpCodes.Call)
+                        {
+                            if (method.Body.Instructions[i + 1].IsLdcI4())
+                                ConstantNum = (method.Body.Instructions[i + 1].GetLdcI4Value());
+                            if (method.Body.Instructions[i + 2].Operand.ToString().Contains(Methoddecryption.ToString()))
+                            {
+                                CilBody body = method.Body;
+                                var string2decrypt = method.Body.Instructions[i].Operand.ToString();
+                                string decryptedstring = null;
+
+                                Assembly assembly = Assembly.LoadFile(textBox1.Text);
+                                Type typez = assembly.GetType(Typedecryption.Name);
+                                if (typez != null)
+                                {
+                                    MethodInfo methodInfo = typez.GetMethod(Methoddecryption.Name,
+                                        BindingFlags.InvokeMethod | BindingFlags.Public  | BindingFlags.Static | BindingFlags.IgnoreCase);
+                                    if (methodInfo != null)
+                                    {
+                                        object result = null;
+                                        ParameterInfo[] parameters = methodInfo.GetParameters();
+                                        if (parameters.Length == 0)
+                                        {
+
+                                        }
+                                        else
+                                        {
+                                            object[] parametersArray = new object[] { string2decrypt, ConstantNum };
+                                            result = methodInfo.Invoke(methodInfo, parametersArray);
+                                            decryptedstring = result.ToString();
+                                            DeobedStringNumber = DeobedStringNumber + 1;
+                                            body.Instructions[i].OpCode = OpCodes.Ldstr;
+                                            body.Instructions[i].Operand = decryptedstring;
+                                            body.Instructions.RemoveAt(i + 1);
+                                            body.Instructions.RemoveAt(i + 1);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
